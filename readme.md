106c24cecab215ec6b2f059aa452f524d1b57724
hery <hhery889@gmail.com>
2023-12-28 16:07:58 +0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Setup
+
+- Launch
+
+```bash
+php -S localhost:9000 -t src
+```
---
+++ src/index.php
@@ -0,0 +1,67 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$GLOBALS['private_key'] = 'path_to_private_key.pem';
+$GLOBALS['public_key'] = 'xxx';
+$GLOBALS['store_id'] = 'xxx';
+
+function get_client()
+{
+  $amazonpay_config = array(
+    'public_key_id' => $GLOBALS['public_key'],
+    'private_key'   => $GLOBALS['private_key'],
+    'region'        => 'EU',
+    'sandbox'       => true,
+    'algorithm' => 'AMZN-PAY-RSASSA-PSS-V2'
+  );
+
+  return new Amazon\Pay\API\Client($amazonpay_config);
+}
+
+function create_checkout_session_payload()
+{
+  $payload = json_encode([
+    'webCheckoutDetails' => [
+      'checkoutReviewReturnUrl' => 'https://a.com/merchant-review-page'
+    ],
+    'storeId' => $GLOBALS['store_id'],
+  ]);
+
+  $client = get_client();
+  $signature = $client->generateButtonSignature($payload);
+
+  return [
+    'payload_json' => $payload,
+    'signature' => $signature
+  ];
+}
+
+$ap_info = create_checkout_session_payload();
+
+?>
+
+<h1>Amazon Pay POC</h1>
+<div id="AmazonPayButton"></div>
+
+<script src="https://static-eu.payments-amazon.com/checkout.js"></script>
+<script type="text/javascript" charset="utf-8">
+  const amazonPayButton = amazon.Pay.renderButton('#AmazonPayButton', {
+    // set checkout environment
+    merchantId: 'merchant_id',
+    publicKeyId: '<?= $GLOBALS['public_key'] ?>',
+    ledgerCurrency: 'EUR',
+    // customize the buyer experience
+    checkoutLanguage: 'en_GB',
+    productType: 'PayAndShip',
+    placement: 'Cart',
+    buttonColor: 'Gold',
+    estimatedOrderAmount: { "amount": "109.99", "currencyCode": "EUR" },
+    // configure Create Checkout Session request
+    createCheckoutSessionConfig: {
+      payloadJSON: '<?= $ap_info['payload_json'] ?>',
+      signature: '<?= $ap_info['signature'] ?>',
+      algorithm: 'AMZN-PAY-RSASSA-PSS-V2'
+    }
+  });
+</script>
